756398cb050d56de5d0e73e6942afc99e835c9ac
Minghui (Matt) Zhu <minghui.zhu@duke.edu>
2024-09-02 21:07:52 +0000

    Fix: Made the default map marker pop up

--- maps/src/components/MapComponent.js
+++ maps/src/components/MapComponent.js
@@ -12,23 +12,27 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
 
-// Component to handle map center when selectedMuseum changes
-const MapCenterHandler = ({ selectedMuseum }) => {
+// Component to handle map center and popup when selectedMuseum changes
+const MapCenterAndPopupHandler = ({ selectedMuseum, markersRef }) => {
   const map = useMap();
 
   useEffect(() => {
-    if (selectedMuseum) {
-      map.flyTo(selectedMuseum.location, 15, {
+    if (selectedMuseum && markersRef.current[selectedMuseum.name]) {
+      const marker = markersRef.current[selectedMuseum.name];
+      map.flyTo(selectedMuseum.location, 14, {
         animate: true,
         duration: 1.5,
       });
+      marker.openPopup();
     }
-  }, [selectedMuseum, map]);
+  }, [selectedMuseum, map, markersRef]);
 
   return null;
 };
 
 const MapComponent = ({ selectedMuseum, museums, onMarkerClick }) => {
+  const markersRef = React.useRef({});
+
   return (
     <MapContainer
       center={selectedMuseum ? selectedMuseum.location : [39.9526, -75.1652]}
@@ -39,8 +43,8 @@ const MapComponent = ({ selectedMuseum, museums, onMarkerClick }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      
-      <MapCenterHandler selectedMuseum={selectedMuseum} />
+
+      <MapCenterAndPopupHandler selectedMuseum={selectedMuseum} markersRef={markersRef} />
 
       {museums.map((museum, index) => (
         <Marker
@@ -48,6 +52,11 @@ const MapComponent = ({ selectedMuseum, museums, onMarkerClick }) => {
           position={museum.location}
           eventHandlers={{
             click: () => onMarkerClick(museum),
+          }}
+          ref={(el) => {
+            if (el) {
+              markersRef.current[museum.name] = el;
+            }
           }}
         >
           <Popup>
